86619419b437592d4d8e801808ff78bb4ef63d40
Mihajlo Colic <colicmihajlo@gmail.com>
2024-12-10 19:16:41 +0100

    Initialized the project repository.

---
+++ src/main/java/mihajlo/svetopismo_web/SvetoPismoWebApplication.java
@@ -0,0 +1,13 @@
+package mihajlo.svetopismo_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvetoPismoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvetoPismoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mihajlo/svetopismo_web/SvetoPismoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package mihajlo.svetopismo_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvetoPismoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
